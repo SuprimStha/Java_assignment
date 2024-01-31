a3c501e7a9a4cb4262f02f72c20d2341361b93b1
suprimshrestha <suprim.shrestha@sifal.deerwalk.edu.np>
2024-01-31 14:35:57 +0545

    assignment

---
+++ src/guessing_game.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class guessing_game {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int targetNumber = random.nextInt(11);
+
+        System.out.println("Welcome to the Guessing Game!");
+
+        while (true) {
+            System.out.print("Enter your guess (1-10): ");
+            int guess = scanner.nextInt();
+
+            if (guess == targetNumber) {
+                System.out.println("Congratulations! You guessed the number.");
+                break;
+            } else if (guess < targetNumber) {
+                System.out.println("Try a higher number.");
+            } else {
+                System.out.println("Try a lower number.");
+            }
+        }
+    }
+
+}
